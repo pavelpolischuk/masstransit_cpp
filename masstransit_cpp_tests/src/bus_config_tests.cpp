db4d5af0219c30ee38ebd270854ab19423cc738a
Tur1st <polischukpv@ya.ru>
2017-08-22 12:29:30 +0300

    Use Catch to test

--- masstransit_cpp_tests/src/bus_config_tests.cpp
+++ masstransit_cpp_tests/src/bus_config_tests.cpp
@@ -1,6 +1,5 @@
 #include "message_consumer_mock.hpp"
-
-#include <gtest/gtest.h>
+#include "catch.hpp"
 
 #include <masstransit_cpp/bus.hpp>
 #include <masstransit_cpp/uri.hpp>
@@ -11,20 +10,6 @@ namespace masstransit_cpp_tests
 {
 	using namespace masstransit_cpp;
 
-	TEST(bus_config_tests, ctor_make_context_info_from_json)
-	{
-		auto consumer_mock = std::make_shared<message_consumer_mock>();
-		uri uri("localhost", "guest", "guest");
-		bus b;
-		b
-			.host(uri, [](send_endpoint& conf) {})
-			.receive_endpoint(uri, "Test.AppName", [=](receive_endpoint & conf)
-		{
-			conf.consumer(consumer_mock);
-			conf.poll_timeout(boost::posix_time::millisec(300));
-		});
-	}
-
 	void clean(uri const& uri, std::set<std::string> const& exchanges, std::set<std::string> const& queues)
 	{
 		auto channel = AmqpClient::Channel::CreateFromUri(uri.to_string());
@@ -34,26 +19,45 @@ namespace masstransit_cpp_tests
 			channel->DeleteQueue(q);
 	}
 
-	TEST(bus_config_tests, send_message_and_receive)
+	TEST_CASE("bus_config_tests", "[bus_config]")
 	{
-		auto consumer_mock = std::make_shared<message_consumer_mock>();
-
-		uri uri("localhost", "guest", "guest");
-		bus b;
-		b
-			.host(uri, [](send_endpoint& conf) {})
-			.receive_endpoint(uri, "Test.AppName", [=](receive_endpoint & conf)
+		SECTION( "ctor_make_context_info_from_json" ) 
 		{
-			conf.consumer(consumer_mock);
-			conf.poll_timeout(boost::posix_time::seconds(6));
-		});
+			auto consumer_mock = std::make_shared<message_consumer_mock>();
+			uri uri("localhost", "guest", "guest");
+			bus b;
+			b
+				.host(uri, [](send_endpoint& conf) {})
+				.receive_endpoint(uri, "Test.AppName", [=](receive_endpoint & conf)
+			{
+				conf.consumer(consumer_mock);
+				conf.poll_timeout(boost::posix_time::millisec(300));
+			});
+    	}
+
+		SECTION("send_message_and_receive")
+		{
+			auto consumer_mock = std::make_shared<message_consumer_mock>();
+
+			uri uri("localhost", "guest", "guest");
+			bus b;
+			b
+				.host(uri, [](send_endpoint& conf) {})
+				.receive_endpoint(uri, "Test.AppName", [=](receive_endpoint & conf)
+			{
+				conf.consumer(consumer_mock);
+				conf.poll_timeout(boost::posix_time::seconds(6));
+			});
 
 
-		b.start();
-		b.publish(message_mock(42));
-		b.stop();
+			b.start();
+			b.publish(message_mock(42));
+			b.stop();
 
-		ASSERT_EQ(consumer_mock->saved_value.get_value_or(0), 42);
-		clean(uri, b.exchanges(), {"Test.AppName"});
+			REQUIRE(consumer_mock->saved_value.get_value_or(0) == 42);
+			
+			clean(uri, b.exchanges(), {"Test.AppName"});
+		}
 	}
+
 }
